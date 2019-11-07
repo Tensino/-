b990bf596ae8b60a9d5a860e29a0a9f9da595ed5
Tensino <love1251284980@vip.qq.com>
2019-11-07 21:58:10 +0900

    first commit

---
+++ cmake-build-debug/CMakeFiles/Sekei.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sekei.dir/main.cpp.obj"
+  "Sekei.exe"
+  "Sekei.exe.manifest"
+  "Sekei.pdb"
+  "libSekei.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sekei.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
